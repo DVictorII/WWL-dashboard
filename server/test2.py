328a346b060b6dd3ab2974423bf2cb471122d98b
Jesusfer1809 <jesusf.rondon1809@gmail.com>
2023-05-24 16:25:39 -0500

    database uploads

---
+++ server/test2.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import utilities2 as uf
+import os
+import psycopg2
+
+dbname = "wwlengineering_rossing"
+user = "WWL_ADMIN"
+password = "WWL#2023"
+host = "wwl-rossing.crnkanilun4m.ap-southeast-2.rds.amazonaws.com"
+dev = True
+
+def dbconnect():
+    conn = psycopg2.connect(host=host, database=dbname, user=user, password=password)
+    return conn
+
+def update(year,month,op=True):
+    try:
+        conn = dbconnect()
+        cur = conn.cursor()
+        
+        query = ""
+        
+        uf.download_data(21545,year,month,option=op)
+        nodes = uf.get_features_from_data(uf.os.path.abspath('data/data_compacted.csv'))
+        print("already got features")
+        uf.save_features(nodes,year,month, cur, query)
+        print("updated readings")
+        uf.save_last_features(nodes, cur, query)
+        print("updated last readings")
+
+        conn.commit()
+
+        """else:
+            name = "data\\compacted-readings-21545-%d-%02d.dat"%(year,month)
+            nodes = uf.get_features_from_data(name)
+            uf.save_features(nodes,year,month)
+            print("done %d %d"%(year,month))"""
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    print(os.path.abspath( "data/calibration_data.csv"))
+    today = datetime.today()
+    print('\n')
+    print('job running: ',today)
+    if today.day == 1:
+        update(today.year,today.month-1,op=True)
+    #current month
+    update(today.year,4,op=True)
+    print("done updated to %d/%d/%d %d"%(today.year,today.month,today.day,today.hour))
--- server/utilities2.py
+++ server/utilities2.py
@@ -19,7 +19,7 @@ host = "wwl-rossing.crnkanilun4m.ap-southeast-2.rds.amazonaws.com"
 dev = True
 
 
-def save_last_features(nodes_df):
+def save_last_features(nodes_df, cur):
     # base = os.getcwd()+"/"
 
     cdf = pd.read_csv(os.path.abspath("data/calibration_data.csv"), index_col="SNumber")
@@ -63,36 +63,28 @@ def save_last_features(nodes_df):
             row = np.array(sub)
 
             try:
-                conn = dbconnect()
-                cur = conn.cursor()
                 data = tuple([idf, k, t, row[-1], row[-2]])
                 query = "INSERT INTO last_readings VALUES (%s,%s,%s,%s,%s) "  # +\
                 # "ON CONFLICT() DO NOTHING;"
                 cur.execute(query, data)
-                conn.commit()
             except:
-                conn = dbconnect()
-                cur = conn.cursor()
                 data = tuple([t, row[-1], row[-2], idf, k])
                 query = (
                     "UPDATE last_readings SET time = %s, pressure = %s, temperature = %s "
                     + "WHERE node=%s AND channel=%s"
                 )
                 cur.execute(query, data)
-                conn.commit()
                 # "ON CONFLICT() DO NOTHING;"
 
 
-def dbconnect():
-    conn = psycopg2.connect(host=host, database=dbname, user=user, password=password)
-    return conn
+# def dbconnect():
+#     conn = psycopg2.connect(host=host, database=dbname, user=user, password=password)
+#     return conn
 
 
-def create_table(id, k):
+def create_table(id, k, cur):
     try:
         # sub = pd.read_csv('download\\node_'+str(id)+'_'+str(k)+'_'+str(year)+'_'+str(month)+'.csv',parse_dates=['TIMESTAMP'])
-        conn = dbconnect()
-        cur = conn.cursor()
         table_name = "node_" + str(id) + "_" + str(k)
         query = (
             "CREATE TABLE IF NOT EXISTS "
@@ -102,7 +94,6 @@ def create_table(id, k):
             + "Pressure numeric(12,6))"
         )
         cur.execute(query)
-        conn.commit()
     except Exception as e:
         print("error in file %d %d - %s" % (id, k, e))
 
@@ -159,12 +150,10 @@ def polynomial2(R, T, S, df):
     return val
 
 
-def copy_data(id, k, year, month):
+def copy_data(id, k, year, month, cur):
     try:
         # sub = pd.read_csv('download\\node_'+str(id)+'_'+str(k)+'_'+str(year)+'_'+str(month)+'.csv',parse_dates=['TIMESTAMP'])
-        create_table(id, k)
-        conn = dbconnect()
-        cur = conn.cursor()
+        create_table(id, k, cur)
         table_name = "node_" + str(id) + "_" + str(k)
 
         file = os.path.abspath("download/node_%d_%d_%d_%02d.csv" % (id, k, year, month))
@@ -178,46 +167,45 @@ def copy_data(id, k, year, month):
                 + "ON CONFLICT (time) DO NOTHING"
             )
             cur.execute(query, tuple(row))
-            conn.commit()
     except Exception as e:
         print("error in file %d %d - %s" % (id, k, e))
 
 
-def update_tables(mysql, id, k, year, month):
-    try:
-        sub = pd.read_csv(
-            os.path.abspath(
-                "download/node_"
-                + str(id)
-                + "_"
-                + str(k)
-                + "_"
-                + str(year)
-                + "_"
-                + str(month)
-                + ".csv"
-            ),
-            parse_dates=["TIMESTAMP"],
-        )
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        query = (
-            "CREATE TABLE IF NOT EXISTS node_"
-            + str(id)
-            + "_"
-            + str(k)
-            + " (timestamp datetime not null primary key,atmpres decimal(10,4),freq decimal(20,5),thermR decimal(20,5),Temperature decimal(10,5),Pressure decimal(10,5));"
-        )
-        cursor.execute(query)
-        conn.commit()
-        table_name = "node_" + str(id) + "_" + str(k)
-        for i, row in sub.iterrows():
-            query = "INSERT IGNORE INTO " + table_name + " VALUES (%s,%s,%s,%s,%s,%s)"
-            cursor.execute(query, tuple(row))
-            conn.commit()
-        print("updated table %d %d" % (id, k))
-    except Exception as e:
-        print("error in file %d %d - %s" % (id, k, e))
+# def update_tables(mysql, id, k, year, month):
+#     try:
+#         sub = pd.read_csv(
+#             os.path.abspath(
+#                 "download/node_"
+#                 + str(id)
+#                 + "_"
+#                 + str(k)
+#                 + "_"
+#                 + str(year)
+#                 + "_"
+#                 + str(month)
+#                 + ".csv"
+#             ),
+#             parse_dates=["TIMESTAMP"],
+#         )
+#         conn = mysql.connect()
+#         cursor = conn.cursor()
+#         query = (
+#             "CREATE TABLE IF NOT EXISTS node_"
+#             + str(id)
+#             + "_"
+#             + str(k)
+#             + " (timestamp datetime not null primary key,atmpres decimal(10,4),freq decimal(20,5),thermR decimal(20,5),Temperature decimal(10,5),Pressure decimal(10,5));"
+#         )
+#         cursor.execute(query)
+#         conn.commit()
+#         table_name = "node_" + str(id) + "_" + str(k)
+#         for i, row in sub.iterrows():
+#             query = "INSERT IGNORE INTO " + table_name + " VALUES (%s,%s,%s,%s,%s,%s)"
+#             cursor.execute(query, tuple(row))
+#             conn.commit()
+#         print("updated table %d %d" % (id, k))
+#     except Exception as e:
+#         print("error in file %d %d - %s" % (id, k, e))
 
 
 def dict_helper(objlist):
@@ -252,7 +240,7 @@ def download_data(gateway, year, month, option=False):
     print("connecting to server")
     print(url)
     response = requests.get(url=url, auth=adm, allow_redirects=True)
-    print("downloaded data from server", response.text)
+    print("downloaded data from server")
     # download data and save it as csv
     open(os.path.abspath("data/data_compacted.csv"), "wb").write(response.content)
     print("downloaded data to local")
@@ -292,7 +280,7 @@ def get_features_from_data(file):
     return nodes_df
 
 
-def save_features(nodes_df, year, month):
+def save_features(nodes_df, year, month, cur):
     cdf = pd.read_csv(os.path.abspath("data/calibration_data.csv"), index_col="SNumber")
     # calculate temperature and pressure by piezometer
     for i in range(len(nodes_df)):
@@ -335,110 +323,110 @@ def save_features(nodes_df, year, month):
                 "download/node_%d_%d_%d_%02d.csv" % (idf, k, year, month)
             )
             sub.to_csv(file, date_format="%Y/%m/%d %H:%M:%S %t")
-            copy_data(idf, k, year, month)
+            copy_data(idf, k, year, month, cur)
 
 
 # download data and process
-def get_data_from_server(mysql, year, month, option=1, cal_params=True):
-    # read data and drop last column
-    print("cleaning data")
-    if option:
-        df = pd.read_csv(
-            os.path.abspath("data/data_compacted.csv"),
-            skiprows=1,
-            parse_dates=["TIMESTAMP"],
-            index_col=0,
-        )
-    else:
-        name = os.path.abspath(
-            "data/compacted-readings-21545-%d-%02d.dat" % (year, month)
-        )
-        df = pd.read_csv(name, skiprows=1, parse_dates=["TIMESTAMP"], index_col=0)
-    df = df.iloc[:, :-1]
-    # counting number of channels per node
-    nodes = dict()
-    for col in df.columns[1:]:
-        n = get_node_id(col)
-        if n in nodes.keys():
-            nodes[n] += 1
-        else:
-            nodes[n] = 1
-    x = list(nodes.items())
-    # saving data per node and per channel
-    nodes_df = []
-    j = 1
-    print("reording")
-    for i in range(len(x)):
-        mainset = df.iloc[:, j : j + x[i][1]]
-        for k in range(0, (x[i][1] - 1) // 3):
-            subset = mainset.iloc[:, [0, 3 * k + 1, 3 * k + 3]]
-            subset = subset.dropna()
-            # cleaning subsets names
-            cols = subset.columns
-            new_cols = []
-            for col in cols:
-                new_cols.append(clean_name(col))
-            subset.columns = new_cols
-            nodes_df.append([x[i][0], k + 1, subset])
-        j += x[i][1]
-    # read piezometer features
-    print("calculating parameters")
+# def get_data_from_server(mysql, year, month, option=1, cal_params=True):
+#     # read data and drop last column
+#     print("cleaning data")
+#     if option:
+#         df = pd.read_csv(
+#             os.path.abspath("data/data_compacted.csv"),
+#             skiprows=1,
+#             parse_dates=["TIMESTAMP"],
+#             index_col=0,
+#         )
+#     else:
+#         name = os.path.abspath(
+#             "data/compacted-readings-21545-%d-%02d.dat" % (year, month)
+#         )
+#         df = pd.read_csv(name, skiprows=1, parse_dates=["TIMESTAMP"], index_col=0)
+#     df = df.iloc[:, :-1]
+#     # counting number of channels per node
+#     nodes = dict()
+#     for col in df.columns[1:]:
+#         n = get_node_id(col)
+#         if n in nodes.keys():
+#             nodes[n] += 1
+#         else:
+#             nodes[n] = 1
+#     x = list(nodes.items())
+#     # saving data per node and per channel
+#     nodes_df = []
+#     j = 1
+#     print("reording")
+#     for i in range(len(x)):
+#         mainset = df.iloc[:, j : j + x[i][1]]
+#         for k in range(0, (x[i][1] - 1) // 3):
+#             subset = mainset.iloc[:, [0, 3 * k + 1, 3 * k + 3]]
+#             subset = subset.dropna()
+#             # cleaning subsets names
+#             cols = subset.columns
+#             new_cols = []
+#             for col in cols:
+#                 new_cols.append(clean_name(col))
+#             subset.columns = new_cols
+#             nodes_df.append([x[i][0], k + 1, subset])
+#         j += x[i][1]
+#     # read piezometer features
+#     print("calculating parameters")
 
-    cdf = pd.read_csv(os.path.abspath("data/calibration_data.csv"), index_col="SNumber")
-    # calculate temperature and pressure by piezometer
-    for i in range(len(nodes_df)):
-        idf = nodes_df[i][0]
-        k = nodes_df[i][1]
-        if cal_params:
-            sub = nodes_df[i][2]
-            if sub.size > 0:
-                feat = cdf.loc[(cdf["Logger"] == idf) & (cdf["Channel"] == k)].fillna(0)
-                if feat.size > 0:
-                    # we suppose the last sensor is the newest one
-                    feat = feat.to_dict("records")[-1]
-                    # get temperature
-                    sub["Temperature"] = sub.apply(
-                        lambda row: get_temp(row["thermRes"], A, B, C), axis=1
-                    )
-                    if feat["G"] != 0:
-                        # get pressure
-                        sub["Pressure"] = sub.apply(
-                            lambda row: polynomial2(
-                                row["freq"],
-                                row["Temperature"],
-                                row["AtmPressure"],
-                                feat,
-                            ),
-                            axis=1,
-                        )
-                    else:
-                        # get pressure
-                        sub["Pressure"] = sub.apply(
-                            lambda row: polynomial1(
-                                row["freq"],
-                                row["Temperature"],
-                                row["AtmPressure"],
-                                feat,
-                            ),
-                            axis=1,
-                        )
-                else:
-                    # get temperature
-                    sub["Temperature"] = sub.apply(lambda row: np.nan, axis=1)
-                    # get pressure
-                    sub["Pressure"] = sub.apply(lambda row: np.nan, axis=1)
-                sub.to_csv(
-                    "download/node_"
-                    + str(idf)
-                    + "_"
-                    + str(k)
-                    + "_"
-                    + str(year)
-                    + "_"
-                    + str(month)
-                    + ".csv",
-                    date_format="%Y/%m/%d %H:%M:%S %t",
-                )
-                update_tables(mysql, idf, k, year, month)
-        else:
-            copy_data(idf, k, year, month)
+#     cdf = pd.read_csv(os.path.abspath("data/calibration_data.csv"), index_col="SNumber")
+#     # calculate temperature and pressure by piezometer
+#     for i in range(len(nodes_df)):
+#         idf = nodes_df[i][0]
+#         k = nodes_df[i][1]
+#         if cal_params:
+#             sub = nodes_df[i][2]
+#             if sub.size > 0:
+#                 feat = cdf.loc[(cdf["Logger"] == idf) & (cdf["Channel"] == k)].fillna(0)
+#                 if feat.size > 0:
+#                     # we suppose the last sensor is the newest one
+#                     feat = feat.to_dict("records")[-1]
+#                     # get temperature
+#                     sub["Temperature"] = sub.apply(
+#                         lambda row: get_temp(row["thermRes"], A, B, C), axis=1
+#                     )
+#                     if feat["G"] != 0:
+#                         # get pressure
+#                         sub["Pressure"] = sub.apply(
+#                             lambda row: polynomial2(
+#                                 row["freq"],
+#                                 row["Temperature"],
+#                                 row["AtmPressure"],
+#                                 feat,
+#                             ),
+#                             axis=1,
+#                         )
+#                     else:
+#                         # get pressure
+#                         sub["Pressure"] = sub.apply(
+#                             lambda row: polynomial1(
+#                                 row["freq"],
+#                                 row["Temperature"],
+#                                 row["AtmPressure"],
+#                                 feat,
+#                             ),
+#                             axis=1,
+#                         )
+#                 else:
+#                     # get temperature
+#                     sub["Temperature"] = sub.apply(lambda row: np.nan, axis=1)
+#                     # get pressure
+#                     sub["Pressure"] = sub.apply(lambda row: np.nan, axis=1)
+#                 sub.to_csv(
+#                     "download/node_"
+#                     + str(idf)
+#                     + "_"
+#                     + str(k)
+#                     + "_"
+#                     + str(year)
+#                     + "_"
+#                     + str(month)
+#                     + ".csv",
+#                     date_format="%Y/%m/%d %H:%M:%S %t",
+#                 )
+#                 update_tables(mysql, idf, k, year, month)
+#         else:
+#             copy_data(idf, k, year, month)
