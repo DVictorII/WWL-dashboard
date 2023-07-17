b94ae114a8d4ecb1f4100cc4d107261240197692
rpoma <raul.pa.93@gmail.com>
2023-07-17 10:51:44 +0800

    Update piezometers_data.py
    Add wwl_functions.py and get_graphics

--- server/routes/piezometers_data.py
+++ server/routes/piezometers_data.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta, date
 from sqlalchemy import text
 import kml2geojson
 import os
+import wwl_functions as wwl
 
 piezometers_data_routes = Blueprint("piezometers_data_routes", __name__)
 
@@ -158,10 +159,10 @@ def get_geojson(folder, name):
 @cross_origin()
 def get_graphics(node, channel):
     try:
-        natural_ground = "data/graphics/natural_ground/"
-        new_ground = "data/graphics/new_ground/"
+        natural_ground = "data/sections/natural_ground/"
+        new_ground = "data/sections/new_ground/"
 
-        pizometers,levels = get_data_by_section(node,channel,natural_ground,new_ground)
+        pizometers,levels = wwl.get_data_by_section(node,channel,natural_ground,new_ground)
         
         return jsonify({"data": pizometers, "name": levels})
 
