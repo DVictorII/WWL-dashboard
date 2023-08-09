3eec74776959752a10cf13bf3619fd3b6630a1af
Jesusfer1809 <jesusf.rondon1809@gmail.com>
2023-08-09 09:44:21 -0500

    b3

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:5000/api/v1"
+    ? "/api/v1"
     : // ? "/api/v1"
       "http://localhost:5000/api/v1";
 
